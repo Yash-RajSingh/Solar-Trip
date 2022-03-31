ccdfdfb8f0232f21005a6ad74b663186ca6c20c6
Yash-RajSingh <yashraj45202@gmail.com>
2022-04-01 04:51:50 +0530

    Fixed mapping error

--- src/components/planets/planetComponent.js
+++ src/components/planets/planetComponent.js
@@ -1,11 +1,11 @@
-export default function PlanetWrapper (name, img, position, description) {
+export default function PlanetWrapper (prop) {
     return(
         <>
         <div className='PlanetWrapper'>
-            <h3>{name}</h3>
-            <img src= {img} alt={name} className="PlanetImage" />
-            <p>{position}</p>
-            <p>{description}</p>
+            <h3>{prop.name}</h3>
+            <img src={prop.img} alt={prop.name} className="PlanetImage" />
+            <p>{prop.position}</p>
+            <p>{prop.description}</p>
         </div>
         </>
     )
